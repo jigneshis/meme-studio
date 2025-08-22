b728a55cfecf64881d393ed528e34eeff9a66960
Jignesh Wadhwani <137609546+jigneshis@users.noreply.github.com>
2025-08-22 18:44:56 +0530

    Create sw.js

---
+++ sw.js
@@ -0,0 +1,33 @@
+// Minimal Service Worker for offline caching
+const CACHE = 'meme-studio-v1';
+const ASSETS = [
+  './',
+  './index.html',
+  './styles.css',
+  './app.js',
+  './manifest.webmanifest',
+  './icons/favicon.svg'
+];
+
+self.addEventListener('install', (e) => {
+  e.waitUntil(caches.open(CACHE).then(cache => cache.addAll(ASSETS)));
+  self.skipWaiting();
+});
+
+self.addEventListener('activate', (e) => {
+  e.waitUntil(
+    caches.keys().then(keys => Promise.all(keys.filter(k=>k!==CACHE).map(k=>caches.delete(k))))
+  );
+  self.clients.claim();
+});
+
+self.addEventListener('fetch', (e) => {
+  const req = e.request;
+  e.respondWith(
+    caches.match(req).then(cached => cached || fetch(req).then(res => {
+      const copy = res.clone();
+      caches.open(CACHE).then(cache => cache.put(req, copy));
+      return res;
+    }).catch(()=>caches.match('./index.html')))
+  );
+});
